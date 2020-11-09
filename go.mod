ba342f5e9a7a7295818d93ec96cea21f6afc3d54
nobody <noreply@github.com>
2020-11-09 23:33:45 +0800

    Handle request in parallel

--- go.mod
+++ go.mod
@@ -13,8 +13,9 @@ require (
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	github.com/wabarc/archive.is v1.0.2
 	github.com/wabarc/archive.org v0.1.1
-	github.com/wabarc/wbipfs v0.1.0
+	github.com/wabarc/wbipfs v0.1.1
 	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
 )
 
 replace github.com/go-shiori/obelisk => github.com/wabarc/obelisk v0.0.0-20200821095614-bffd0d7f6be7
--- telegram.go
+++ telegram.go
@@ -2,11 +2,13 @@ package wayback
 
 import (
 	"bytes"
+	"context"
 	"log"
 	"regexp"
 	"text/template"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/sync/errgroup"
 )
 
 type collect struct {
@@ -32,52 +34,70 @@ func (cfg *Config) Telegram() {
 		log.Panic(err)
 	}
 
+	g, _ := errgroup.WithContext(context.Background())
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
 
+		update := update
 		text := update.Message.Text
-		uri := []string{}
+		urls := []string{}
 		re := regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 		match := re.FindAllString(text, -1)
 		for _, el := range match {
-			uri = append(uri, el)
+			urls = append(urls, el)
 		}
-		if len(uri) == 0 {
+		if len(urls) == 0 {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "URL no found.")
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
 			continue
 		}
 
-		c := &collect{}
-		h := Handle{URI: uri}
-		for hd, do := range cfg.handler {
-			switch {
-			case hd == "ia" && do:
-				c.Arc = append(c.Arc, "<a href='https://web.archive.org/'>Internet Archive</a>")
-				c.Dst = append(c.Dst, h.IA())
-			case hd == "is" && do:
-				c.Arc = append(c.Arc, "<a href='https://archive.today/'>Archive Today</a>")
-				c.Dst = append(c.Dst, h.IS())
-			case hd == "ip" && do:
-				h.IPFS = cfg.IPFS
-				c.Arc = append(c.Arc, "<a href='https://ipfs.github.io/public-gateway-checker/'>IPFS</a>")
-				c.Dst = append(c.Dst, h.WBIPFS())
+		g.Go(func() error {
+			msgID, archives, err := cfg.archive(update.Message.MessageID, urls)
+			if err != nil {
+				return err
 			}
-		}
 
-		replyText := message(c)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, replyText)
-		msg.ReplyToMessageID = update.Message.MessageID
-		msg.ParseMode = "html"
-
-		bot.Send(msg)
-
-		if len(cfg.ChatID) > 0 {
-			msg = tgbotapi.NewMessageToChannel("@"+cfg.ChatID, replyText)
+			replyText := message(archives)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, replyText)
+			msg.ReplyToMessageID = msgID
 			msg.ParseMode = "html"
+
 			bot.Send(msg)
+
+			if len(cfg.ChatID) > 0 {
+				msg = tgbotapi.NewMessageToChannel("@"+cfg.ChatID, replyText)
+				msg.ParseMode = "html"
+				bot.Send(msg)
+			}
+			return nil
+		})
+	}
+}
+
+func (cfg *Config) archive(msgid int, urls []string) (int, *collect, error) {
+	c := &collect{}
+
+	h := Handle{URI: urls}
+	for hd, do := range cfg.handler {
+		switch {
+		case hd == "ia" && do:
+			c.Arc = append(c.Arc, "<a href='https://web.archive.org/'>Internet Archive</a>")
+			c.Dst = append(c.Dst, h.IA())
+		case hd == "is" && do:
+			c.Arc = append(c.Arc, "<a href='https://archive.today/'>Archive Today</a>")
+			c.Dst = append(c.Dst, h.IS())
+		case hd == "ip" && do:
+			h.IPFS = cfg.IPFS
+			c.Arc = append(c.Arc, "<a href='https://ipfs.github.io/public-gateway-checker/'>IPFS</a>")
+			c.Dst = append(c.Dst, h.WBIPFS())
 		}
 	}
+
+	return msgid, c, nil
 }
 
 func message(vars *collect) string {
