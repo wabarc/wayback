ad1569d7a81c0cc24177694774d5b1a5017bd547
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2023-05-03 15:21:54 +0100

    Add doc.go

---
+++ ingress/doc.go
@@ -0,0 +1,23 @@
+// Copyright 2023 Wayback Archiver. All rights reserved.
+// Use of this source code is governed by the GNU GPL v3
+// license that can be found in the LICENSE file.
+
+/*
+Package ingress provides functionality for registering services.
+
+The ingress package allows you to register wayback services.
+
+To use the ingress package, import the package and its dependencies,
+such as register, as shown in the following example:
+
+	package main
+
+	import (
+	        _ "github.com/wabarc/wayback/ingress"
+	)
+
+	func main() {
+	        // ...
+	}
+*/
+package ingress // import "github.com/wabarc/wayback/ingress"
