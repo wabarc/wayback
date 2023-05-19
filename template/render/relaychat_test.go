66ff4dace33bf0e9e3a899fe8016c900460ee5c7
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2023-05-19 17:00:09 +0100

    Fix testing

--- template/render/relaychat_test.go
+++ template/render/relaychat_test.go
@@ -9,14 +9,20 @@ import (
 )
 
 func TestRenderForIRC(t *testing.T) {
-	expected := `Internet Archive:
+	expected := `‹ Example ›
+ 
+***** List of Archives *****
+ 
+Internet Archive:
 • https://web.archive.org/web/20211000000001/https://example.com/
 archive.today:
 • http://archive.today/abcdE
 IPFS:
 • https://ipfs.io/ipfs/QmTbDmpvQ3cPZG6TA5tnar4ZG6q9JMBYVmX2n3wypMQMtr
 Telegraph:
-• http://telegra.ph/title-01-01`
+• http://telegra.ph/title-01-01
+ 
+***** End of Archives *****`
 
 	got := ForPublish(&Relaychat{Cols: collects, Data: bundleExample}).String()
 	if got != expected {
