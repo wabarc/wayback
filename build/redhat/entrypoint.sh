b4039a22313e5bf1895948050523b05799b0a665
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2024-03-27 05:18:59 +0000

    Add license header

--- build/redhat/entrypoint.sh
+++ build/redhat/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright 2024 Wayback Archiver. All rights reserved.
+# Use of this source code is governed by the GNU GPL v3
+# license that can be found in the LICENSE file.
 
 set -eu pipefail
 
