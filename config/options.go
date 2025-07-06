29cb8d2f62dfd78245d42f9eba0369c0697df05f
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2025-07-06 13:07:08 +0000

    Rename privacy url

--- config/options.go
+++ config/options.go
@@ -105,7 +105,7 @@ const (
 
 	defOmnivoreApikey = ""
 
-	defPrivacyURL = "https://privacy.wabarc.eu.org"
+	defPrivacyURL = "https://wabarc.eu.org/privacy"
 
 	maxAttachSizeTelegram = 50000000   // 50MB
 	maxAttachSizeDiscord  = 8000000    // 8MB
